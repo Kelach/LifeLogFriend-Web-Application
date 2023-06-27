18c52e5fda6ce23208dd00653feaa197a6822e29
Kelechi <kelechi@utexas.edu>
2023-06-27 15:17:11 -0700

    setup psql database creation

---
+++ lifetracker-api/lifetracker-schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY, 
+    password TEXT NOT NULL, 
+    username TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NULL NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE nutrition(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    calories TEXT NOT NULL,
+    image_url TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+-- CREATE TABLE sleep(
+--     id SERIAL PRIMARY KEY,
+--     name TEXT NOT NULL,
+--     category TEXT NOT NULL,
+--     calories TEXT NOT NULL,
+--     image_url TEXT NOT NULL,
+--     user_id TEXT NOT NULL,
+--     created_at TIMESTAMP NOT NULL,
+-- )
+-- CREATE TABLE exercise(
+--     id SERIAL PRIMARY KEY,
+--     name TEXT NOT NULL,
+--     category TEXT NOT NULL,
+--     calories TEXT NOT NULL,
+--     image_url TEXT NOT NULL,
+--     user_id TEXT NOT NULL,
+--     created_at TIMESTAMP NOT NULL,
+-- )
