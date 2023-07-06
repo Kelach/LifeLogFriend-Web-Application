aac91be95c6bf747756aa732e805627fb846e1c7
Kelechi <kelechi@utexas.edu>
2023-07-06 07:52:34 -0700

    add optional labels and helper text feature

--- lifetracker-ui/src/components/TextInput/TextInput.jsx
+++ lifetracker-ui/src/components/TextInput/TextInput.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
 import "./TextInput.css";
-export default function TextInput(props){
-    // const {name = "text", onValueChange, value, placeholder, type, style} = props;
+export default function TextInput({ helperText, showLabel, name, ...props }) {
+    // renders basic text input with optional helper text and label
     return (
-        <input 
-        className="input-text"
-        {...props}/>
+        <>
+            <div className="text-input-container">
+                {showLabel ? <label htmlFor={name.toLowerCase()} className="text-input-label">{name.toLowerCase()}:</label> : <></>}
+                <div className="text-input-box">
+                    <input
+                        name={name}
+                        {...props} />
+                    <p className={"input-helper-text" + (helperText?.expression ? " show" : "")}>
+                        Input must {helperText?.constraint}
+                    </p>
+                </div>
+            </div>
+        </>
     )
 }
