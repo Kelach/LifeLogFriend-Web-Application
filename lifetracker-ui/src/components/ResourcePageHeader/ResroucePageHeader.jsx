bb91eec7eb7ea111edee88de44da7a455573efec
Kelechi <kelechi@utexas.edu>
2023-07-06 15:31:52 -0700

    improved styling and general functionality

--- lifetracker-ui/src/components/ResourcePageHeader/ResroucePageHeader.jsx
+++ lifetracker-ui/src/components/ResourcePageHeader/ResroucePageHeader.jsx
@@ -1,8 +1,12 @@
 import "./ResourcePageHeader.css";
-export default function ResourcePageHeader({ title, imagePath }) {
+import testImage from "../../assets/food.jpg";
+export default function ResourcePageHeader({ title, imageFileName, imageAlt }) {
+    // all images must be located in the assets folder
+    const basePATH = "/src/assets/";
     return (
         <div className="resource-header-section">
             <h1 className="resource-header-section-title">{title || "No Title Provided" }</h1>
+            <img src={basePATH + imageFileName} alt={imageAlt} />
         </div>
     )
 }
--- lifetracker-ui/src/components/TextInput/TextInput.jsx
+++ lifetracker-ui/src/components/TextInput/TextInput.jsx
@@ -1,16 +1,18 @@
 import "./TextInput.css";
 export default function TextInput({ helperText, showLabel, name, ...props }) {
+    const labelName = name.replace("_", " ").toLowerCase();
+    
     // renders basic text input with optional helper text and label
     return (
         <>
             <div className="text-input-container">
-                {showLabel ? <label htmlFor={name.toLowerCase()} className="text-input-label">{name.toLowerCase()}:</label> : <></>}
+                {showLabel ? <label htmlFor={name} className="text-input-label">{labelName}:</label> : <></>}
                 <div className="text-input-box">
                     <input
                         name={name}
                         {...props} />
                     <p className={"input-helper-text" + (helperText?.expression ? " show" : "")}>
-                        Input must {helperText?.constraint}
+                        <span style={{textTransform: "capitalize"}}>{labelName}</span> must {helperText?.constraint}
                     </p>
                 </div>
             </div>
