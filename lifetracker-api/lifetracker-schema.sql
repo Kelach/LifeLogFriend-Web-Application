54b9b8faf30cd402e109007f4562d8cce374f148
Kelechi <kelechi@utexas.edu>
2023-07-09 22:01:20 -0700

    add sleep table

--- lifetracker-api/lifetracker-schema.sql
+++ lifetracker-api/lifetracker-schema.sql
@@ -32,12 +32,13 @@ CREATE TABLE exercise(
     user_id TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
--- CREATE TABLE sleep(
---     id SERIAL PRIMARY KEY,
---     name TEXT NOT NULL,
---     category TEXT NOT NULL,
---     calories TEXT NOT NULL,
---     image_url TEXT NOT NULL,
---     user_id TEXT NOT NULL,
---     created_at TIMESTAMP NOT NULL,
--- );
+CREATE TABLE sleep(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    startTime TIMESTAMP NOT NULL,
+    endTime TIMESTAMP NOT NULL,
+    duration INT NOT NULL,
+    userId TEXT NOT NULL,
+    createdAt TIMESTAMP DEFAULT NOW(),
+);
