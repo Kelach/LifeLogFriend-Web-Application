e15fe2aad6c8fdfe287a1f02103205a68bd122c0
Kelechi <kelechi@utexas.edu>
2023-07-09 18:46:28 -0700

    implement following feature connected w/ backend

--- lifetracker-ui/src/pages/UsersPage/UsersPage.jsx
+++ lifetracker-ui/src/pages/UsersPage/UsersPage.jsx
@@ -7,13 +7,13 @@ import { getTimeFromTimestamp } from "../../utils/timeUtils.js";
 function UserTableRow({ user, isFollowing, handleUserFollowClick }) {
     
     return (
-        <div className="user-row">
+        <div className="user-row light-text-hover">
             <div className="user-details">
                 <span>{user.firstName} {user.lastName}</span>
                 <span>{getTimeFromTimestamp(user.createdAt)}</span>
             </div>
-            <button onClick={(event) => handleUserFollowClick(event, user.email)} className={"follow-btn btn-outline-small" + (isFollowing ? " following" : "")} >
-                <span className="material-icons">person_add</span>
+            <button title={"Follow " + user.firstName} onClick={(event) => handleUserFollowClick(event, user.email)} className={"follow-btn btn-outline-small" + (isFollowing ? " following" : "")} >
+                <span className="material-icons">{isFollowing ? "how_to_reg" : "person_add"}</span>
             </button>
         </div>
     )
@@ -23,54 +23,81 @@ function UserTableRow({ user, isFollowing, handleUserFollowClick }) {
 export default function UsersPage({ user }) {
 
     const [users, setUsers] = useState([]);
-    const [userFollowing, setUserFollowing] = useState({});
+    const [userFollowing, setUserFollowing] = useState(new Set());
     const [inputValue, setInputValue] = useState("");
 
     const handleUserFollowClick = async (event, otherUserId) => {
         event.preventDefault();
         // function toggles between unfollowing and folowing users  
-        console.log("other id on button press: ", otherUserId), event;
-        // if (otherUserId in userFollowing){
-        //     // unfollows user
-        //     const payload = {
-        //         key : "follows_id",
-        //         value : otherUserId
-        //     }
-        //     const {data, success, statusCode} = await ApiClient.deleteEntry("following", payload)
-        //     if (success){
-        //         console.log("successfully unfollowed user: ", user.id)
-        //     }else{
-        //         console.log("unable to unfollow user: ", user.firstName, " ", user.lastName)
-        //     }
-        // }else{
-        //     // follows user
-        //     const payload = {
-        //         user_id: user.email,
-        //         follows_id : otherUserId
-        //     }
-        //     const {data, success, statusCode} = await ApiClient.postEntry("following", payload)
-        //     if (success){
-        //         console.log("successfully folowed user: ", user.id)
-        //     }else{
-        //         console.log("unable to follow user: ", user.firstName, " ", user.lastName)
-        //     }
+        console.log("other id on button press: ", otherUserId);
+        console.log(userFollowing.has(otherUserId), userFollowing)
+        if (userFollowing.has(otherUserId)){
+            // update state first
+            const prevFollowing = userFollowing
+            userFollowing.delete(otherUserId);
+            const newFollowing = new Set(userFollowing)
+            setUserFollowing(newFollowing); 
 
-        // }
+            // then update following in db
+            const {data, success, statusCode} = await ApiClient.unfollowUser(otherUserId);
+            if (success){
+                console.log("successfully unfollowed user: ", otherUserId);
+                // removing user from set
+            }else{
+                console.log("unable to unfollow user with id: ", otherUserId);
+                setUserFollowing(prevFollowing); 
+
+            }
+        }else{
+            // update state first
+            const prevFollowing = userFollowing
+            userFollowing.add(otherUserId);
+            const newFollowing = new Set(userFollowing)
+            setUserFollowing(newFollowing); 
+
+            // then update following in db
+            const {data, success, statusCode} = await ApiClient.followUser(otherUserId)
+            if (success){
+
+                console.log("successfully folowed user with id: ", otherUserId)
+            }else{
+                console.log("unable to follow user with id: ", otherUserId);
+                setUserFollowing(prevFollowing); 
+
+            }
+
+        }
 
     }
 
     useEffect(() => {
+        // first fetch all users
         ApiClient.fetchAllUsers().then((response) => {
-            console.log("users fetched: ", response)
-            setUsers(response.data.users)
+            if (response.success){
+                console.log("users fetched: ", response);
+                setUsers(response.data.users);
+            } else{
+                console.log("recieved error repsonse while trying to fetch all users: ", response);
+            }
+        }).catch((error) => {
+            console.log("error caught while fetching for all users: ", error);
         })
-        // console.log("get users here");
-    }, []);
 
+        // then fetch following for given user
+        ApiClient.fetchFollowing().then((response) => {
+            if (response.success){
+                console.log("user following fetched: ", response);
+                setUserFollowing(new Set(response.data.following));
+            } else{
+                console.log("recieved error repsonse while trying to fetch user following: ", response);
+            }
+        }).catch((error) => {
+            console.log("error caught while fetching user following: ", error);
+        })
+    }, []);
     return (
         <>
             <div className="users-container">
-
                 <div className="users-table">
                     <h1 className="light-text-hover">Users<i className="material-icons-outlined">emoji_emotions</i></h1>
                     <div className="user-table-header-row">
@@ -81,7 +108,7 @@ export default function UsersPage({ user }) {
 
                         // const isFollowing = @TODO
                         return (
-                            <UserTableRow handleUserFollowClick={handleUserFollowClick} user={user} isFollowing={false} />
+                            <UserTableRow handleUserFollowClick={handleUserFollowClick} user={user} isFollowing={userFollowing.has(user.email)} />
                         )
                     }) : (
                         <h1>No Users Found...</h1>
